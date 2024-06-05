0ec839144ca7505d42e60f99e97c4e4762460569
Paul H <humphreyspaul020672@gmail.com>
2024-06-05 11:23:24 +0100

    Added a hello to Ann

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
+        System.out.println(sayHelloToSomeone("Ann"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Paul"));
